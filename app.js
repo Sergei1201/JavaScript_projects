d16b1b29a7135afb61c11be2e8fa6b26cec594fc
Sergei Sokolov <sokolov12ssa@gmail.com>
2021-09-09 21:09:37 +0500

    Observing patterns

--- app.js
+++ app.js
@@ -176,6 +176,113 @@
 //     member.define();
 // })
 
+// function EventObserver() {
+//     this.observers = [];
+// }
+// EventObserver.prototype = {
+//     subscribe: function(fn) {
+//         this.observers.push(fn);
+//         console.log(`You are now subscribed to ${fn.name}`)
+//     },
+
+//     unsubscribe: function(fn) {
+//         this.observers = this.observers.filter(function(item) {
+//     if(item !== fn) {
+//         return item;
+//     }
+//         });
+//         console.log(`You are now unsubscribed from ${fn.name}`);
+//     },
+
+//     fire: function() {
+//         this.observers.forEach(function(item) {
+//             item.call();
+//         })
+//     }
+// }
+
+// const click = new EventObserver();
+// // Event listeners
+// // Subscribe event
+// document.querySelector('.sub-ms').addEventListener('click', function() {
+//     click.subscribe(getCurMilliSeconds);
+// });
+// // Unsubscribe event
+// document.querySelector('.unsub-ms').addEventListener('click', function() {
+//     click.unsubscribe(getCurMilliSeconds);
+// });
+// // Fire event
+// document.querySelector('.fire').addEventListener('click', function() {
+//     click.fire();
+// })
+
+// document.querySelector('.sub-s').addEventListener('click', function() {
+//     click.subscribe(getCurSeconds);
+// });
+
+// document.querySelector('.unsub-s').addEventListener('click', function() {
+//     click.unsubscribe(getCurSeconds);
+// })
+
+// //Click handler
+// const getCurMilliSeconds = function() {
+//     console.log(`Current milliseconds: ${new Date().getMilliseconds()}`);
+// }
+
+// const getCurSeconds = function() {
+//     console.log(`Current seconds: ${new Date().getSeconds()}`);
+// }
+
+
+
+
+// Observer patterns
+
+
+function ObserverPattern() {
+    this.observers = [];
+}
+
+ObserverPattern.prototype = {
+    subscribe: function(fn) {
+        this.observers.push(fn);
+        console.log(`You are now subscribed to ${fn.name}`);
+    },
+
+    unsubscribe: function(fn) {
+        this.observers = this.observers.filter(function(item) {
+            if (item !== fn) {
+                return item;
+            }
+            console.log(`You are now unsubscribed from ${fn.name}`)
+        })
+    },
+
+    fire: function() {
+        this.observers.forEach(function(item) {
+            item.call();
+        })
+    }
+}
+
+// Creating event listeners
+
+const observer = new ObserverPattern();
+document.querySelector('.sub-ms').addEventListener('click', function() {
+    observer.subscribe(subscribeMilliSeconds);
+})
+document.querySelector('.unsub-ms').addEventListener('click', function() {
+    observer.unsubscribe(subscribeMilliSeconds);
+})
+
+document.querySelector('.fire').addEventListener('click', function() {
+    observer.fire();
+})
+
+const subscribeMilliSeconds = function() {
+    console.log(`${new Date().getMilliseconds()}`);
+}
+
 
 
 
